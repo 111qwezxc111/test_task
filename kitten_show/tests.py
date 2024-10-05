378fe9aa00cd59be3fbf1a86d1303fec94be2770
kifusss <kifus_06@mail.ru>
2024-10-05 22:42:38 +0300

    add tests

--- kitten_show/tests.py
+++ kitten_show/tests.py
@@ -17,11 +17,11 @@ class KittensTestCase(APITestCase):
             color='test',
             age=10,
             breed='test',
-            description='test'
+            description='test',
+            owner=self.user
         )
 
     def test_kitten_create(self):
-
         data = {
             'name': self.kitten.name,
             'color': self.kitten.color,
@@ -40,4 +40,86 @@ class KittensTestCase(APITestCase):
             status.HTTP_201_CREATED
         )
 
+        self.assertEqual(
+            response.json(),
+            {'id': 3, 'name': 'test', 'color': 'test', 'age': 10,
+             'breed': 'test', 'description': 'test'}
+        )
 
+    def test_breeds(self):
+        response = self.client.get(
+            '/kitten_show/breeds/'
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK
+        )
+
+        self.assertEqual(
+            response.json(),
+            [{'id': 1, 'name': 'test', 'breed': 'test'}]
+        )
+
+    def test_kittens(self):
+        response = self.client.get(
+            '/kitten_show/kittens/'
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK
+        )
+
+        self.assertEqual(
+            response.json(),
+            [{'id': 7, 'name': 'test', 'color': 'test', 'age': 10,
+              'breed': 'test', 'description': 'test'}]
+        )
+
+    def test_kitten_view(self):
+        response = self.client.get(
+            f'/kitten_show/kitten_view/{self.kitten.id}/'
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK
+        )
+
+        self.assertEqual(
+            response.json(),
+            {'id': 6, 'name': 'test', 'color': 'test', 'age': 10,
+             'breed': 'test', 'description': 'test'}
+        )
+
+    def test_kitten_update(self):
+        data = {
+            'name': 'qwe',
+        }
+
+        response = self.client.patch(
+            f'/kitten_show/kitten_update/{self.kitten.id}/',
+            data=data
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK
+        )
+
+        self.assertEqual(
+            response.json(),
+            {'id': 5, 'name': 'qwe', 'color': 'test', 'age': 10,
+             'breed': 'test', 'description': 'test'}
+        )
+
+    def test_kitten_delete(self):
+        response = self.client.delete(
+            f'/kitten_show/kitten_delete/{self.kitten.id}/'
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_204_NO_CONTENT
+        )
